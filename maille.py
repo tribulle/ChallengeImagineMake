d95f5004a7366cb7eb6512d04729668c3be9aaef
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-03 10:00:07 +0200

    Ajout de la fonction set_angle_dist
    Ajout aussi de commentaires

--- maille.py
+++ maille.py
@@ -1,3 +1,8 @@
+"""
+Cette classe permet de définir des mailles (sets de 3 points)
+pour créer une géométrie de base
+"""
+
 class maille:
     def __init__(self):
         self.points = []
--- point.py
+++ point.py
@@ -1,3 +1,9 @@
+import math
+
+
+"""
+Cette classe permet de définir des points dans l'espace (en récupérant la sortie du système)
+"""
 class point:
     def __init__(self):
         self.x = 0
@@ -7,3 +13,13 @@ class point:
         self.x = x
         self.y = y
         self.z = z
+    """
+    dist : la distance retournée par du capteur
+    theta : l'angle "horizontal" (l'angle de rotation du lidar)
+    phti : l'angle "vertical" (celui du moteur supplémentaire)
+    """
+    def set_angle_dist(self,dist,theta,phi):
+        self.x = dist*math.cos(theta)*math.sin(phi)
+        self.y = dist*math.sin(phi)*math.sin(theta)
+        self.z = dist*math.cos(theta)
+
