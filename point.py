f132829314dc709f28704e17edc8a8112c566051
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-03 09:42:04 +0200

    Create point.py

---
+++ point.py
@@ -0,0 +1,9 @@
+class point:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.y = 0 
+    def set(self, x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
