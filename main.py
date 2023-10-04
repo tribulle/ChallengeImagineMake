5b277716f4d133810f06a4d20582bb30594d292a
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-04 09:18:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ espace_in = espace.espace()
 data = []
 for i in range(2500):
     data.append(random_point())
+
 #Le carré
 data = []
 data.append([-1,-1,-1])
@@ -27,7 +28,7 @@ data.append([1,1,1])
 data.append([1,1,-1])
 data.append([1,-1,1])
 
-espace_in.init_points(data,True)
+espace_in.init_points(data,False)
 #espace_in.afficher()
 
 data_np = espace_in.return_xyz()
