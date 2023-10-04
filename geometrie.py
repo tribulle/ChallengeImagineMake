de6343fcd92b9a69dcbdbb64c7d882381f95693c
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-04 14:46:10 +0200

    Ajout du produit vectoriel et du vecteur normal

--- geometrie.py
+++ geometrie.py
@@ -11,7 +11,7 @@ class geometrie:
         return math.sqrt((x1.x-x2.x)**2+(x1.y-x2.y)**2+(x1.z-x2.z)**2)
     
     """
-    Définit 
+    Définit le produit scalaire entre de vecteurs de la classe vecteur
     """
     @staticmethod
     def produit_scalaire(v1,v2):
@@ -22,6 +22,19 @@ class geometrie:
         norme = math.sqrt(geometrie.produit_scalaire(v,v))
         return vecteur.vecteur(v.x/norme,v.y/norme,v.z/norme)
     
+    """
+    Retourne le produit scalaire de deux vecteurs
+    """
+    @staticmethod
+    def produit_vectoriel(v1,v2):
+        x = v1.y*v2.z-v1.z*v2.y
+        y = v1.z*v2.x-v1.x*v2.z
+        z = v1.x*v2.y - v1.y*v2.x
+        return vecteur(x,y,z)
+
+    """
+    Retourne le vecteur normal normalisé d'une maille
+    """
     @staticmethod
     def vecteur_normal(m):
         if(len(m.liste_points)!=3):
@@ -30,4 +43,10 @@ class geometrie:
         x = m.liste_points[0]
         y = m.liste_points[1]
         z = m.liste_points[2]
+        v1 = vecteur.vecteur(0,0,0)
+        v2 = vecteur.vecteur(0,0,0)
+        v1.vect_directeur(x,y)
+        v2.vect_directeur(y,z)
+        return geometrie.normaliser(geometrie.produit_vectoriel(v1,v2))
+        
 
