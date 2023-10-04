5caaf18a998ffec352eab51d6ed414e76296ff14
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-04 16:21:27 +0200

    Correction erreur

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import figures_test
 
 espace_in = espace.espace()
 data = figures_test.figure_test.sphere(2500,100)
-espace_in.init_points(data,True)
+espace_in.init_points(data,False)
 #espace_in.afficher()
 
 data_np = espace_in.return_xyz()
