91510fa89fa1bc6d575f53b8e4593c159b069b7f
lucarne14 <146843230+lucarne14@users.noreply.github.com>
2023-10-04 12:33:22 +0200

    Update geometrie.py

--- geometrie.py
+++ geometrie.py
@@ -7,3 +7,9 @@ class geometrie:
     @staticmethod
     def dist_eucl(x1,x2):
         return math.sqrt((x1.x-x2.x)**2+(x1.y-x2.y)**2+(x1.z-x2.z)**2)
+    
+    @staticmethod
+    def produit_scalaire(v1,v2):
+        return math.abs(v1.x*v2.x + v1.y*v2.y + v1.z*v2.z)
+    
+
