69086257604ca6daa197b151166f8aaccdaeb364
riken95 <128236100+riken95@users.noreply.github.com>
2023-10-05 12:39:19 +0200

    //

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import figures_test
 
 espace_in = espace.espace()
 data = figures_test.figure_test.sphere(2500,100)
-#data = figures_test.figure_test.cube(1)
-espace_in.init_points(data,False)
+data = figures_test.figure_test.cube(1)
+espace_in.init_points(data,True)
 #espace_in.afficher()
 
 data_np = espace_in.return_xyz()
